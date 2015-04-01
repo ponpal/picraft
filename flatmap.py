ce942dbe2cd5fab3ac45abb405742b845e7a5dea
ponpal <pallp@student.chalmers.se>
2015-04-01 16:26:18 +0200

    Flatmap updated

--- flatmap.py
+++ flatmap.py
@@ -6,11 +6,9 @@ mc = minecraft.Minecraft.create()
 mc.postToChat("Connected")
 
 def main():
-	mc.postToChat("Removing obstacles")
-	mc.setBlocks(-128, 0, -128, 128, 100, 128, block.AIR.id)
-	mc.postToChat("Creating floor")
+	mc.setBlocks(-128, 0, -128, 128, 64, 128, block.AIR.id)
 	mc.setBlocks(-128, -5, -128, 128,   0, 128, 155)
-	mc.postToChat("Flatmap created")
+	mc.postToChat("Flatmap created.")
 
 if __name__ == '__main__':
 	main()
